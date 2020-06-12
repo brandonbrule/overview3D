c2335276bcbd9377f2174bccad59f41422528193
Brandon Brule <brandonbrule@gmail.com>
2020-06-11 20:06:44 -0400

    Jumping

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 12f;
     public float gravity = -9.81f;
 
+    public float jumpHeight = 3f;
+
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
@@ -33,6 +35,13 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        Debug.Log(isGrounded);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
         velocity.y += gravity * Time.deltaTime;
 
