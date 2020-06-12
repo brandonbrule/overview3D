387c95600eef3a0cd3224200284901f28f594606
Brandon Brule <brandonbrule@gmail.com>
2020-06-11 20:15:27 -0400

    unity scene isgrounded detection

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -36,8 +36,6 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
-        Debug.Log(isGrounded);
-
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
