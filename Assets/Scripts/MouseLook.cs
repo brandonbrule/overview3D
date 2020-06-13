20bf723f02298f06eb933cfa54b59f4bdf48c2e6
Brandon Brule <brandonbrule@gmail.com>
2020-06-13 19:01:29 -0400

    locked camera angle

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -23,7 +23,14 @@ public class MouseLook : MonoBehaviour
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        if (xRotation >= 0f && xRotation <= 13f)
+        {
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            
+        }
+
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
