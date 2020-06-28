036710cd858d69bc78147fb84e47d65436e5e865
Brandon Brule <brandonbrule@gmail.com>
2020-06-28 12:47:16 -0400

    A little doggy that follows you

---
+++ Assets/FollowAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowAI : MonoBehaviour
+{
+    //Transform that NPC has to follow
+    public Transform transformToFollow;
+    //NavMesh Agent variable
+    UnityEngine.AI.NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Follow the player
+        agent.destination = transformToFollow.position;
+    }
+}
